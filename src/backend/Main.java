3f674b2e9ad4a918b960525c06994d7e82269d74
Manojkumar <manojkumar18102006@gmail.com>
2025-09-20 21:55:31 +0530

    main completed

--- src/backend/Main.java
+++ src/backend/Main.java
@@ -1,5 +1,9 @@
 package backend;
 
-public class Main {
-    
+import frontend.GameFrame;
+
+public class Main{
+    public static void main(String[] args) {
+        new GameFrame();
+    }
 }
--- src/frontend/GamePanel.java
+++ src/frontend/GamePanel.java
@@ -5,8 +5,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
 
-public class GamePanel extends JPanel implements ActionListener {
-
+public class GamePanel extends JPanel implements ActionListener{
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;  // size of each square
@@ -83,7 +82,7 @@ public class GamePanel extends JPanel implements ActionListener {
             running = false;
         }
     }
-    
+
     public void checkFood() {
         if (x[0] == foodX && y[0] == foodY) {
             bodyParts++;
